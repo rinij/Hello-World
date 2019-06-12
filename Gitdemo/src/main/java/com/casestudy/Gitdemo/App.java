1fd70e917a2389cd9d1a3fd476b12037b6320546
rinij <rinij@rinij.cybage.com>
2019-06-12 18:15:07 +0530

    Divya Test1

--- Gitdemo/src/main/java/com/casestudy/Gitdemo/App.java
+++ Gitdemo/src/main/java/com/casestudy/Gitdemo/App.java
@@ -6,14 +6,13 @@ import java.util.Scanner;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = a+b;
-    	System.out.println( "Addition="+c);
-    }
+public class App {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = a + b;
+		System.out.println("Addition=" + c);
+		//test 123
+	}
 }
