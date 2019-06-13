ac6016c2fe35a279273a738d22341e4cc0153433
Rini Jose <rinij@cybage.com>
2019-06-13 19:16:30 +0530

    we are commiting the inital content

--- MavenDemo/src/test/java/com/casestudy/MavenDemo/Junit_test.java
+++ MavenDemo/src/test/java/com/casestudy/MavenDemo/Junit_test.java
@@ -8,7 +8,9 @@ public class Junit_test extends TestCase {
 	
 	@Test
 	public void test(){
+		
 	Even_num e = new Even_num();
-	assertEquals("Number is even", true, e.is_Even(10));
+	assertEquals("Number is even", true, e.is_Even(2));
+	System.out.println("Number is even");
 	}
 }
