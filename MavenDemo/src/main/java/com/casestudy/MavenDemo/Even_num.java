d659e8c900e7f509f12b5b61326c8f81a52d4abc
rinij <rinij@rinij.cybage.com>
2019-06-13 18:03:51 +0530

    skjfdsfjg

---
+++ MavenDemo/src/main/java/com/casestudy/MavenDemo/Even_num.java
@@ -0,0 +1,15 @@
+package com.casestudy.MavenDemo;
+
+public class Even_num {
+	
+	
+	public boolean is_Even(int num){
+		
+		if (num % 2 == 0)
+			return true;
+		else
+			return false;
+	}
+	
+}
+	
---
+++ MavenDemo/src/test/java/com/casestudy/MavenDemo/Junit_test.java
@@ -0,0 +1,14 @@
+package com.casestudy.MavenDemo;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class Junit_test extends TestCase {
+	
+	@Test
+	public void test(){
+	Even_num e = new Even_num();
+	assertEquals("Number is even", true, e.is_Even(10));
+	}
+}
